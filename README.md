510695b2970cacc8efa6f3d4813844c7443a6d8d
JMaylor <joseph.maylor@gmail.com>
2020-10-17 17:59:51 +0100

    init commit, before npm packaging

---
+++ README.md
@@ -0,0 +1,5 @@
+# mapbox-vue-form-template
+
+## Work in Progress
+
+This will be a re-usable location selection form component using mapbox, fitting the Vuetify theme.
---
+++ src/App.vue
@@ -0,0 +1,35 @@
+<template>
+	<div id="app">
+		<Form
+			:geo="true"
+			:initialLocation="[-0.496934, 51.437032]"
+			:apiKey="apiKey"
+		/>
+	</div>
+</template>
+
+<script>
+	import Form from "./components/Form.vue";
+
+	export default {
+		name: "App",
+		components: {
+			Form,
+		},
+		computed: {
+			apiKey() {
+				return process.env.VUE_APP_MAPBOX_KEY;
+			},
+		},
+	};
+</script>
+
+<style>
+	#app {
+		font-family: Avenir, Helvetica, Arial, sans-serif;
+		-webkit-font-smoothing: antialiased;
+		-moz-osx-font-smoothing: grayscale;
+		color: #2c3e50;
+		margin-top: 60px;
+	}
+</style>
